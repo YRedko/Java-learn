43914feb7f5ae75307b9b72d532d7b440119e7b7
yredko <wizardcattus@gmail.com>
2018-10-26 15:36:07 +0300

    lesson 1

---
+++ src/lesson1/exercise2.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+import java.util.Scanner;
+
+public class exercise2 {
+    public static void main(String args[]){
+        int money;
+        Scanner in = new Scanner(System.in);
+        System.out.println("-_-_-");
+        money=in.nextInt();
+        money=money+2;
+        System.out.println(money);
+    }
+}
